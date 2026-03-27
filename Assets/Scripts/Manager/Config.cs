db1f96444f662e0e298c446d8ee199b0a98310a4
WuTong142857 <2827353739@qq.com>
2026-03-27 17:25:12 +0800

    222

---
+++ Assets/Scripts/Manager/Config.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+public class Config//配置各声音
+{
+    public const string bgm1 = "Audio/Music/bgm1";
+    public const string btn_click = "Audio/Sound/buttonclick";
+    public const string eat = "Audio/Sound/chompsoft";
+    public const string lastWave = "Audio/Sound/hugewave";
+    public const string lose_music = "Audio/Sound/losemusic";
+    public const string win_music = "Audio/Sound/winmusic";
+    public const string plant = "Audio/Sound/plant";
+    public const string shoot = "Audio/Sound/shoot";
+}
