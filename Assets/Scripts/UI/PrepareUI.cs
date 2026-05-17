af5eee07d0a3fbc1e3aeb5cc9725bf29df8558a7
WuTong142857 <2827353739@qq.com>
2026-05-17 17:34:10 +0800

    ok

---
+++ Assets/Scripts/UI/PrepareUI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PrepareUI : MonoBehaviour
+{
+    private Animator animator;
+
+    private Action onComplete;
+    private void Start() {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+
+    }
+    public void Show(Action action)
+    {
+        onComplete = action;
+        animator.enabled = true;
+    }
+    void OnShowComplete()
+    {
+        onComplete?.Invoke();
+    }
+    // Start is called before the first frame update
+   
+}
